68bce23e35298bda05617f87a766c6eeaed901c6
arifitantotito <arifitantotito@gmail.com>
2022-04-08 16:06:53 +0700

    pinia-mine

--- pinia-exc/src/views/login/index.vue
+++ pinia-exc/src/views/login/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useAuthStore } from '@/stores/auth';
+import { useAuthStore } from '../../stores/auth';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 const email = ref('');
